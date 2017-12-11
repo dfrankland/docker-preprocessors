eb423ff847d4387c6282493ff52fb583c82051da
Dylan Frankland <dylan@frankland.io>
2017-12-10 20:28:59 -0800

    Fix up and simplify rollup-plugin-docker

--- packages/rollup-plugin-docker/src/index.js
+++ packages/rollup-plugin-docker/src/index.js
@@ -1,29 +1,7 @@
 import { writeFileSync } from 'fs';
 import { dirname, resolve as resolvePath } from 'path';
-import { randomBytes } from 'crypto';
 import { createFilter } from 'rollup-pluginutils';
 import dockerPreprocessor from 'docker-preprocessor';
-
-const virtualFile = () => {
-  const PREFIX = `\0docker-${randomBytes(32).toString('hex')}:`;
-  const SUFFIX = '.wasm';
-
-  return {
-    matchesVirtualFile: id => id.startsWith(PREFIX) && id.endsWith(SUFFIX),
-    createVirtualFileId: id => `${PREFIX}${id}${SUFFIX}`,
-    revertVirtualFileId: id => (
-      id.replace(new RegExp(`^\\${PREFIX}(.*?)\\${SUFFIX}$`), '$1')
-    ),
-  };
-};
-
-const ast = {
-  type: 'Program',
-  sourceType: 'module',
-  start: 0,
-  end: null,
-  body: [],
-};
 
 export default ({
   include,
@@ -31,23 +9,15 @@ export default ({
   options,
 } = {}) => {
   const filter = createFilter(include, exclude);
-  const {
-    matchesVirtualFile,
-    createVirtualFileId,
-    revertVirtualFileId,
-  } = virtualFile();
 
   const plugin = {
     name: 'docker',
-    resolveId: (id, importer = '') => {
-      const fullPath = resolvePath(dirname(importer), id);
-      if (matchesVirtualFile(id) || !filter(fullPath)) return null;
-      return createVirtualFileId(fullPath);
-    },
-    load: async function dockerTransform(id) {
-      if (!matchesVirtualFile(id)) return null;
+
+    transform: async function dockerTransform(code, id) {
+      if (!filter(id)) return null;
+
       try {
-        const result = await dockerPreprocessor(options)(revertVirtualFileId(id));
+        const result = await dockerPreprocessor(options)(id);
 
         const {
           error,
@@ -68,7 +38,7 @@ export default ({
           });
         };
 
-        return { code: content.toString('binary'), map: sourceMap, ast };
+        return { code: content.toString('binary'), map: sourceMap };
       } catch (err) {
         return this.error(err);
       }
